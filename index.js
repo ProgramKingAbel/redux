abc85090eebea899146d34151a6e6ef781868bb2
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-26 14:07:56 +0100

    Define initial state- redux principle

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const orderCake = () => {
 
     return {
         type: CAKE_ORDERED,
-        quantity: 1
+        quantity: 1,
     }
 }
+
+const initialState = {
+    numOfCakes: 10,
+}
+
+// (previousState, action) => newState; ----------reducers
