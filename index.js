1268c279e9031ce9307080499237f2ce4d7a2b93
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-26 14:02:32 +0100

    Implement Action creator function

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const CAKE_ORDERED = 'CAKE_ORDERED';
 
-{
-    type: CAKE_ORDERED
-    // quantity: 1
+const orderCake = () => {
+
+    return {
+        type: CAKE_ORDERED,
+        quantity: 1
+    }
 }
