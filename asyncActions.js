073791f0f5910df22e1c3707f62367c1e38fe241
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-26 22:03:11 +0100

    Implement Action Creator functions

--- asyncActions.js
+++ asyncActions.js
@@ -11,4 +11,24 @@ const FETCH_USERS_REQUESTED = 'FETCH_USERS_REQUESTED';
 const FETCH_USERS_SUCCEEDED = 'FETCH_USERS_SUCCEEDED';
 const FETCH_USERS_FAILED = 'FETCH_USERS_FAILED';
 
-//create action functions 
+//create action creator functions 
+const fetchUsersRequest = () => {
+    return {
+        type: FETCH_USERS_REQUESTED,
+    }
+}
+
+const fetchUsersSuccess = users => {
+    return {
+        type: FETCH_USERS_SUCCEEDED,
+        payload: users,
+    }
+}
+
+const fetchUsersFailed = error => {
+    return {
+        type: FETCH_USERS_FAILED,
+        payload: error,
+    }
+}
+
