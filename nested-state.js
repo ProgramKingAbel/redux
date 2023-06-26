764a281289474afe22eb022224346c83e229b754
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-26 21:13:58 +0100

    Barebones nested state

---
+++ nested-state.js
@@ -0,0 +1,44 @@
+const redux = require('redux');
+const createStore = redux.createStore;
+
+const initialState = {
+    name: 'Abel',
+    address: {
+        street: '123 Main Street',      
+        city: 'Boston',
+        state: 'MA',
+    }
+}
+
+const STREET_UPDATED = 'STREET_UPDATED';
+
+const updateStreet = (street) => {
+    return {
+        type: STREET_UPDATED,
+        payload: street
+    }
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case STREET_UPDATED:
+            return {
+                ...state,
+                address: {
+                    ...state.address,
+                    street: action.payload,
+                }
+            }
+        default: {
+            return state
+        }
+        
+    }
+    
+}
+
+const store = redux.createStore(reducer);
+console.log('Initial State', store.getState());
+const unsubscribe = store.subscribe(() => console.log('Updated state', store.getState()));
+store.dispatch(updateStreet('Washington'));
+unsubscribe();
