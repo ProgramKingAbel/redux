fc669bf299a2e35bf9ef1f0204c344df23c4d4ce
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-26 14:25:47 +0100

    Configure redux to hold app state -- createStore(); --depracated

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const redux = require('redux');
+const createStore = redux.createStore;
+
 const CAKE_ORDERED = 'CAKE_ORDERED';
 
 const orderCake = () => {
@@ -24,3 +27,5 @@ const reducer = (state = initialState, action) => {
             return state;
     }
 }
+
+const store = createStore(reducer);
