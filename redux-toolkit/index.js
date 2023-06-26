98cf2d6a0cc292e58d91cf78b687098035d90fba
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-27 00:57:14 +0100

    Dispatch actions

--- redux-toolkit/index.js
+++ redux-toolkit/index.js
@@ -1,6 +1,14 @@
 const store = require('./App/store');
+const cakeActions = require('./App/features/cake/cakeSlice').cakeActions;
 
 console.log('Initial State', store.getState())
 const unsubscribe = store.subscribe(() => {
     console.log('UpdatedState', store.getState())
 })
+
+store.dispatch(cakeActions.ordered())
+store.dispatch(cakeActions.ordered())
+store.dispatch(cakeActions.ordered())
+store.dispatch(cakeActions.restoked(3))
+
+unsubscribe()
