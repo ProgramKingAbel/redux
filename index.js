801ddfd9ab323a9b2db1ea534722e38335762cb0
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-26 14:56:36 +0100

    Rename quantity to payload

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ const orderCake = () => {
 
     return {
         type: CAKE_ORDERED,
-        quantity: 1,
+        payload: 1,
     }
 }
 
 const restockCake = (qty=1) => {
     return {
         type: CAKE_RESTOCKED,
-        quantity: qty,
+        payload: qty,
     }
 }
 
@@ -35,7 +35,7 @@ const reducer = (state = initialState, action) => {
         case CAKE_RESTOCKED:
             return {
                 ...state,
-                numOfCakes: state.numOfCakes + action.quantity,
+                numOfCakes: state.numOfCakes + action.payload,
             }
         default:
             return state;
