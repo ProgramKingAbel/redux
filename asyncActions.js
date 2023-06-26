a9a1909ba85a287970977db0ab65466c6282ee26
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-26 21:54:59 +0100

    Declare actions

--- asyncActions.js
+++ asyncActions.js
@@ -1,5 +1,14 @@
+const redux = require('redux');
+
 const initialState = {
     loading: false,
     users: [],
     error: '',
 }
+
+//Declare actions
+const FETCH_USERS_REQUESTED = 'FETCH_USERS_REQUESTED';
+const FETCH_USERS_SUCCEEDED = 'FETCH_USERS_SUCCEEDED';
+const FETCH_USERS_FAILED = 'FETCH_USERS_FAILED';
+
+//create action functions 
