627400aca8998866dd3015ffc620e5cf28dafbd2
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-27 11:47:03 +0100

    Import and init logger

--- redux-toolkit/App/store.js
+++ redux-toolkit/App/store.js
@@ -1,6 +1,9 @@
 const configureStore = require('@reduxjs/toolkit').configureStore;
 const cakeReducer = require('./features/cake/cakeSlice');
 const icecreamReducer = require('./features/icecream/icecreamSlice');
+const reduxLogger = require('redux-logger');
+
+const logger = reduxLogger.createLogger();
 
 const store = configureStore({
     reducer: {
