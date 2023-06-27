1807267126d68aedbd13ed2751e1eb1a8b63f9ac
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-27 16:18:47 +0100

    dispatch fetch function

--- redux-toolkit/index.js
+++ redux-toolkit/index.js
@@ -1,19 +1,22 @@
 const store = require('./App/store');
 const cakeActions = require('./App/features/cake/cakeSlice').cakeActions;
 const icecreamActions = require('./App/features/icecream/icecreamSlice').icecreamActions;
+const fetchUsers = require('./App/features/user/userSlice').fetchUsers;
 
 console.log('Initial State', store.getState())
 const unsubscribe = store.subscribe(() => {
     console.log('Updated state', store.getState());
 })
 
-store.dispatch(cakeActions.ordered())
-store.dispatch(cakeActions.ordered())
-store.dispatch(cakeActions.ordered())
-store.dispatch(cakeActions.restocked(3))
+store.dispatch(fetchUsers());
 
-store.dispatch(icecreamActions.ordered())
-store.dispatch(icecreamActions.ordered())
-store.dispatch(icecreamActions.restocked(2))
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.restocked(3))
+
+// store.dispatch(icecreamActions.ordered())
+// store.dispatch(icecreamActions.ordered())
+// store.dispatch(icecreamActions.restocked(2))
 
 unsubscribe()
