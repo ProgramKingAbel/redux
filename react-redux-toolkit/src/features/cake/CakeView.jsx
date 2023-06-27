66758aa8c0499cbaed71ecc5c3809a4fbf421771
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-27 17:58:38 +0100

    Create rafc and import in app

---
+++ react-redux-toolkit/src/features/cake/CakeView.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const CakeView = () => {
+  return (
+      <div>
+          <h2>Number of Cakes - </h2>
+          <button type="button">Order cake</button>
+          <button type="button">Restock cakes</button>
+    </div>
+  )
+}
---
+++ react-redux-toolkit/src/features/icecream/IcecreamView.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const IcecreamView = () => {
+  return (
+    <div>
+          <h2>Number of Ice creams - </h2>
+          <button type="button">Order icecreams</button>
+          <button type="button">Restock ice creams</button>
+    </div>
+  )
+}
