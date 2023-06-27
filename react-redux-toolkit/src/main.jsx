482fdca59017966e751587f89fe1dc35f65ce6fe
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-27 18:14:59 +0100

    Pass store as prop to provider

--- react-redux-toolkit/src/main.jsx
+++ react-redux-toolkit/src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
+import store from './App/store.js'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
     <App />
     </Provider>
   </React.StrictMode>,
