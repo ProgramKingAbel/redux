3cfb806bf10510f5febed61c0140e567f2fbdfa8
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-27 22:06:59 +0100

    Add local state required only within component

--- react-redux-toolkit/src/features/cake/CakeView.jsx
+++ react-redux-toolkit/src/features/cake/CakeView.jsx
@@ -4,13 +4,20 @@ import { ordered, restocked } from './cakeSlice'
 
 
 export const CakeView = () => {
+    const [value, setValue] = React.useState(1);
+
     const numOfCakes = useSelector((state) => state.cake.numOfCakes)
     const dispatch = useDispatch();
   return (
       <div>
           <h2>Number of Cakes - { numOfCakes }</h2>
-          <button type="button" onClick={()=> {dispatch(ordered())}}>Order cake</button>
-          <button type="button" onClick={()=> {dispatch(restocked(5))}}>Restock cakes</button>
+          <button type="button" onClick={() => { dispatch(ordered()) }}>Order cake</button>
+          <input
+              type='number'
+              value={value}
+              onChange={(e) => setValue(parseInt(e.target.value))}
+          />
+          <button type="button" onClick={()=> {dispatch(restocked(value))}}>Restock cakes</button>
     </div>
   )
 }
--- react-redux-toolkit/src/features/icecream/IcecreamView.jsx
+++ react-redux-toolkit/src/features/icecream/IcecreamView.jsx
@@ -2,14 +2,23 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { ordered, restocked } from './icecreamSlice'
 
+
 export const IcecreamView = () => {
+
+    const [value, setValue] = React.useState(1);
+
     const numOfIceCreams = useSelector((state) => state.icecream.numOfIceCreams)
     const dispatch = useDispatch();
   return (
     <div>
           <h2>Number of Ice creams - { numOfIceCreams }</h2>
-          <button onClick={()=> dispatch(ordered())} type="button">Order icecreams</button>
-          <button onClick={()=> dispatch(restocked(5))} type="button">Restock ice creams</button>
+          <button onClick={() => dispatch(ordered())} type="button">Order icecreams</button>
+          <input
+              type='number'
+              value={value}
+              onChange={(e) => setValue(parseInt(e.target.value))}
+          />
+          <button onClick={()=> dispatch(restocked(value))} type="button">Restock ice creams</button>
     </div>
   )
 }
