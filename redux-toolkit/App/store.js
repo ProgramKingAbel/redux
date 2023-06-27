33b709f2420f3e2dd650118cc74207073fac1610
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-27 11:34:53 +0100

    Import Ice cream Reducer

--- redux-toolkit/App/store.js
+++ redux-toolkit/App/store.js
@@ -1,5 +1,6 @@
 const configureStore = require('@reduxjs/toolkit').configureStore;
 const cakeReducer = require('./features/cake/cakeSlice');
+const icecreamReducer = require('./features/icecream/icecreamSlice');
 
 const store = configureStore({
     reducer: {
