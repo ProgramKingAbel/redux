ae33e53ace24018dabcb73a192938aab61bb8d41
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-27 22:24:46 +0100

    implement User jsx

--- react-redux-toolkit/src/features/user/UserView.jsx
+++ react-redux-toolkit/src/features/user/UserView.jsx
@@ -3,13 +3,28 @@ import { useDispatch, useSelector } from 'react-redux'
 import { fetchUsers } from './userSlice.js'
 
 export const UserView = () => {
-const dispatch = useDispatch();
+const dispatch = useDispatch()
+const user = useSelector(state => state.user)
 
   useEffect(()=>{
     dispatch(fetchUsers());
   }, [])
 
   return (
-    <h2>List of Users</h2>
+  <div>
+    <h2 >List of Users</h2>
+{ user.loading && <div>Loading ... </div> }
+{ !user.loading && user.error ? <div>Error: {user.error}</div> : null }
+{
+  !user.loading && user.users.length ? (
+    <ul>
+      {
+        user.users.map(user => (
+          <li key={user.id}>{ user.name }</li>
+        ))
+      }
+  </ul>
+    ) :null }
+  </div>
   )
 }
