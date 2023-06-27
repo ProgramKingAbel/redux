7d59d5e13b9e2fff23c9cb0fbfc7e4765a03cab7
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-27 16:14:44 +0100

    Create Async Thunkfunction

--- redux-toolkit/App/features/user/userSlice.js
+++ redux-toolkit/App/features/user/userSlice.js
@@ -1,6 +1,6 @@
 const createSlice = require('@reduxjs/toolkit').createSlice;
 const createAsyncThunk = require('@reduxjs/toolkit').createAsyncThunk;
-
+const axios = require('axios');
 
 const initialState = {
     loading: false,
@@ -8,7 +8,33 @@ const initialState = {
     error: '',
 }
 
+//Generates pending, fullfilled and rejected action types
+
+const fetchUsers = createAsyncThunk('user/fetchUsers', () => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.data.map((user) => user.id))
+})
+
 const userSlice = createSlice({
     name: 'user',
     initialState,
+    extraReducers: builder => {
+        builder.addCase(fetchUsers.pending, state => {
+            state.loading = true
+        })
+        builder.addCase(fetchUsers.fulfilled, (state, action) => {
+            state.loading = false
+                state.users = action.payload
+                state.error = ''
+        })
+        builder.addCase(fetchUsers.rejected, (state, action) => {
+            state.loading = false
+            state.users = []
+            state.error = action.error.message
+
+        })
+    }
 });
+
+module.exports = userSlice.reducer;
+module.exports.fetchUsers = fetchUsers;
