61726e7b8e875ed53baec6a617f36c041ee615f8
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-27 22:16:27 +0100

    call dispatch from userEffect Hook

--- react-redux-toolkit/src/features/user/UserView.jsx
+++ react-redux-toolkit/src/features/user/UserView.jsx
@@ -1,6 +1,14 @@
 import React, { useEffect} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchUsers } from './userSlice.js'
 
 export const UserView = () => {
+const dispatch = useDispatch();
+
+  useEffect(()=>{
+    dispatch(fetchUsers());
+  }, [])
+
   return (
     <h2>List of Users</h2>
   )
